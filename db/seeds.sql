715e3594bb13c5d1aa5b1d2b2c123ce4eedd6f3a
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-09 18:25:18 -0500

    adding starter code

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers VALUES (1, "Jalepeno Smoked Burger", false);
+INSERT INTO burgers VALUES (2, "Bacon Only Burger", false);
+INSERT INTO burgers VALUES (3, "Bean Dip Burger", false);
