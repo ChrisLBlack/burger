bab31a739a96c1dd55693c8f6b0066bf0be134e4
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-10 20:41:36 -0500

    setting up handlebars code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 //Create all routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
-    burger.all((data) => {
+    burger.selectAll((data) => {
         const burgObj = {
             burgers: data
         };
