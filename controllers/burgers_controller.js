86eff9390a04b7723ec917210a8d9a9017e0f0e9
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-10 21:13:52 -0500

    adding css

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -13,6 +13,29 @@ router.get("/", (req, res) => {
         console.log(burgObj);
         res.render("index", burgObj)
     });
+
+    // post request to add a new burger and set its devoured value to false 
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(["burger_name", "devoured"], [
+      req.body.burger_name, false
+    ], function(result) {
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  // put request to update the burger that was clicked in the list to be devoured: true
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 });
 
 module.exports = router;
