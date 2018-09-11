d563caa7266ac5a1e5e8da5929ca69254fddc193
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-09-10 20:07:41 -0500

    getting app to start working

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
+//Dependencies
 const express = require("express");
 const bodyParser = require("body-parser");
+//PORT for server to "plug" into
+const PORT = process.env.PORT || 8080;
+//naming express npm
+const app = express();
+//using public folder for handlebars
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+//requiring handle-bars npm
+const exhbs = require("express-handlebars");
+app.engine("handlebars", exhbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+//requiring routes
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+//server is listening
+app.listen(PORT, () =>{
+    console.log("Server listening on: http://localhost:" + PORT)
+});
